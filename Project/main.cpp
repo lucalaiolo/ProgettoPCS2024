41c92f5c527bd988007dfff76806236bc88afa31
lucalaiolo <s296054@studenti.polito.it>
2024-05-06 16:55:58 +0200

    Luca first commit

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,8 +1,10 @@
 #include "Polygons.hpp"
 #include "Eigen/Eigen"
 #include "UCDUtilities.hpp"
-
+#include <iostream>
+using namespace std;
 int main()
 {
+    cout << "Ciao" << endl;
     return 0;
 }
